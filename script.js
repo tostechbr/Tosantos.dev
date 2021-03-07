9414154c53db19530326e7de0f814f5e2cde9dad
tosantos1 <tiagolivesan@outlook.com>
2021-03-07 13:19:33 -0300

    Portfólio tosantos.dev

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-$(document).ready(function(){
-    $(".hamburguer").click(function(){
-        $(this).toggleClass("active");
-        $(".menu").toggleClass("active");
-    });
-});
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId);
+    const nav = document.getElementById(navId);
+
+    if(toggle && nav){
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('menu-mobile');
+            toggle.classList.toggle('active-bx');
+        })
+    }
+}
+
+showMenu('bx', 'menu-mobile')
